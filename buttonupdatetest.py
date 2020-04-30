f8f5272a2469127985f8883d5de3ad629df19206
Pete Newman <pwn394@newcastle.edu.au>
2020-05-01 09:06:44 +1000

    Testing updating button text

---
+++ buttonupdatetest.py
@@ -0,0 +1,19 @@
+import PySimpleGUI as sg
+
+layout = [[sg.Button('Start',key='toggleStart'), sg.Cancel()]]
+
+window = sg.Window("Test Update", layout)
+begin = window['toggleStart']
+StartPause = {True : "Start", False : "Pause"}
+Started = True
+
+while True:
+    event, values = window.Read(timeout = 1000)
+    print(f"event: {event}, values: {values}")
+    if event in (None, "Cancel"):
+        break
+    elif event == "toggleStart":
+        Started = not Started
+        begin.update(text=StartPause[Started])
+
+window.close()
